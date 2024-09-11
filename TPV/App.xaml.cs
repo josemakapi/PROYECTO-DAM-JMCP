8f0167ec476ea44a186c81dfecbdc7a73af3758a
José Manuel Calles Piñas <josemankapi@hotmail.com>
2024-09-11 18:27:43 +0200

    Creación de proyecto en VS

---
+++ TPV/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TPV
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TPV/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TPV
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
